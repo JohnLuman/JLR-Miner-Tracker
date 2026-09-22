a4805c103716e7da45738f81f7002d86bb747d51
JohnLuman <mcglashan.cameron@gmail.com>
2026-09-22 03:13:36 -0700

    Bump Tracker assets to 2.9.67

--- public/tracker.js
+++ public/tracker.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
-  const ALARM_VERSION='2.9.66';
-  const CORE_URL='/tracker-core.js?v=2.9.66';
+  const ALARM_VERSION='2.9.67';
+  const CORE_URL='/tracker-core.js?v=2.9.67';
 
   let alarmContext=null;
   let alarmSource=null;
