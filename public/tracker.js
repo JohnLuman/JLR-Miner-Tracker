6153a2abb1b7baec4fb089d4b83d07b75141ae9e
JohnLuman <mcglashan.cameron@gmail.com>
2026-09-22 02:59:29 -0700

    Bump Tracker voice assets to 2.9.63

--- public/tracker.js
+++ public/tracker.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
-  const ALARM_VERSION='2.9.62';
-  const CORE_URL='/tracker-core.js?v=2.9.62';
+  const ALARM_VERSION='2.9.63';
+  const CORE_URL='/tracker-core.js?v=2.9.63';
 
   let alarmContext=null;
   let alarmSource=null;
