2ce6a09f59215292a542577542e2664acc2b260f
JohnLuman <mcglashan.cameron@gmail.com>
2026-09-23 11:42:55 -0700

    Bump Tracker runtime to v2.9.125

--- public/tracker.js
+++ public/tracker.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
-  const ALARM_VERSION='2.9.124';
-  const CORE_URL='/tracker-core.js?v=2.9.124';
+  const ALARM_VERSION='2.9.125';
+  const CORE_URL='/tracker-core.js?v=2.9.125';
 
   let alarmContext=null;
   let alarmSource=null;
