3fb4185ca4d48d538af7c024422a3e130c36f6a4
JohnLuman <mcglashan.cameron@gmail.com>
2026-09-23 19:21:02 -0700

    Bump Tracker regional intel release to 2.9.134

--- public/tracker.js
+++ public/tracker.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
-  const ALARM_VERSION='2.9.133';
-  const CORE_URL='/tracker-core.js?v=2.9.133';
+  const ALARM_VERSION='2.9.134';
+  const CORE_URL='/tracker-core.js?v=2.9.134';
 
   let alarmContext=null;
   let alarmSource=null;
