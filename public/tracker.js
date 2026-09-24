2b03b1d5279180a83fd0b082040d3464f11ba706
JohnLuman <mcglashan.cameron@gmail.com>
2026-09-24 02:30:18 -0700

    Bust Tracker core cache for themed hourly selector

--- public/tracker.js
+++ public/tracker.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
   const ALARM_VERSION='2.9.144';
-  const CORE_URL='/tracker-core.js?v=2.9.144';
+  const CORE_URL='/tracker-core.js?v=2.9.144-system-pass1';
 
   let alarmContext=null;
   let alarmSource=null;
