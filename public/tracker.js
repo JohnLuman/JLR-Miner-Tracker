01a95709928142009051ea1e89ed564fcd431251
JohnLuman <mcglashan.cameron@gmail.com>
2026-09-23 21:32:46 -0700

    Bump Tracker cache version to 2.9.140

--- public/tracker.js
+++ public/tracker.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
-  const ALARM_VERSION='2.9.139';
-  const CORE_URL='/tracker-core.js?v=2.9.139';
+  const ALARM_VERSION='2.9.140';
+  const CORE_URL='/tracker-core.js?v=2.9.140';
 
   let alarmContext=null;
   let alarmSource=null;
